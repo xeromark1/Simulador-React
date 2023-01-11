d29a570bdbd24afa54a285d0dbe7c3a13c18ecf6
xeromark1 <122186114+xeromark1@users.noreply.github.com>
2023-01-10 22:06:24 -0300

    Add files via upload

---
+++ src/Logic/Table.jsx
@@ -0,0 +1,33 @@
+//import {GenerarTabla} from './Logica';
+import {GenerarTabla} from '../GenerarTabla';
+import {LoadDoc} from './LoadDoc';
+
+
+export function Table(){
+    return(
+
+
+
+        <div className="container" >
+                
+                    <div className="ComboBoxCarrera">
+
+                        <LoadDoc/>         
+
+                    </div>          
+                
+                
+                    <div>
+                        <div id = "ComboBox" className="ComboBox">
+                
+                        </div> 
+                
+                        <GenerarTabla/>
+                </div>          
+                
+                
+        </div>
+    
+    
+    );
+}
